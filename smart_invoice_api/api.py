24d0d8bcf49632740f3be52ee8e2f6666fc62999
dawoodjee <adam.daveed@gmail.com>
2024-09-24 16:53:27 +0200

    sync request generated by api request, calls vsdc

--- smart_invoice_api/api.py
+++ smart_invoice_api/api.py
@@ -4,21 +4,39 @@ import requests
 
 
 
+# called from smart_invoice_app / rest api
 @frappe.whitelist()
 def select_codes():
     data = frappe.request.json
     end_point = "/code/selectCodes"
-    data = data = {
+    data = {
         "tpin": data["tpin"],
         "bhfId": data["bhf_id"],
         "lastReqDt": "20240902151722"
     }
-    return call_vsdc(end_point, data)
+    return create_sync_request(end_point, data)
 
+# creating a sync request doc triggers the call to vsdc
+def create_sync_request(end_point, data):
+    try:
+        sr = frappe.get_doc({
+            "doctype": "Sync Request",
+            "attempts": 0,
+            "end_point": end_point,
+            "status": "New",
+            "doc_owner": frappe.session.user,
+            "request_data": data            
+        })
+        sr.insert(ignore_permissions=True)   
+        frappe.db.commit() 
+    except Exception as e:
+        frappe.log_error(frappe.get_traceback(), "Error creating Sync Request")
+        return {"error": str(e)}
+
+# to be called from sync_request doctype
 def call_vsdc(end_point, data):
     settings = get_settings()
     base_url = settings.base_url
-
     try:
         r = requests.post(base_url + end_point, json=data, headers={"Content-Type": "application/json"})
         response_json = r.json()
@@ -35,3 +53,10 @@ def get_settings():
     if not settings.base_url or not settings.environment:
         frappe.throw("VSDC Settings are incomplete. The admin will be notified.")
     return settings
+
+
+def get_status(response):
+    if response.get("resultCd"):
+        return "Success"
+    else:
+        return "Error"
--- smart_invoice_api/smart_invoice_api/doctype/sync_request/sync_request.py
+++ smart_invoice_api/smart_invoice_api/doctype/sync_request/sync_request.py
@@ -3,11 +3,29 @@
 
 import frappe
 from frappe.model.document import Document
+from smart_invoice_api.api import call_vsdc, get_status
 
 
+"""
+Sync Request created {'resultCd': '000', 'resultMsg': 'It is succeeded', 'resultDt': '20240924163305', 'data': {'clsList': [{'cdCls': '06', 'cdClsNm': 'Sale category', 'userDfnNm1': None, 'dtlList': [{'cd': '4', 'cdNm': 'RVAT', 'userDfnCd1': None}]}, {'cdCls': '10', 'cdClsNm': 'Quantity Unit', 'userDfnNm1': None, 'dtlList': [{'cd': 'P1', 'cdNm': 'Pack', 'userDfnCd1': None}, {'cd': 'EA', 'cdNm': 'Each', 'userDfnCd1': None}, {'cd': 'PL', 'cdNm': 'Pallet', 'userDfnCd1': None}, {'cd': 'EACH', 'cdNm': 'Each', 'userDfnCd1': None}, {'cd': 'Ft', 'cdNm': 'Feet', 'userDfnCd1': None}, {'cd': 'MM', 'cdNm': 'Millimetre', 'userDfnCd1': None}, {'cd': 'In', 'cdNm': 'Inches', 'userDfnCd1': None}, {'cd': 'Oz', 'cdNm': 'Ounce', 'userDfnCd1': None}, {'cd': 'YR', 'cdNm': 'Year', 'userDfnCd1': None}, {'cd': 'M ', 'cdNm': 'Month', 'userDfnCd1': None}, {'cd': 'Wk', 'cdNm': 'Week', 'userDfnCd1': None}, {'cd': 'D', 'cdNm': 'Day', 'userDfnCd1': None}, {'cd': 'hr', 'cdNm': 'Hour', 'userDfnCd1': None}, {'cd': 'ha', 'cdNm': 'Hectare', 'userDfnCd1': None}, {'cd': 'yd2', 'cdNm': 'Square yards', 'userDfnCd1': None}, {'cd': 'ft2', 'cdNm': 'Square feet', 'userDfnCd1': None}, {'cd': 'cm2', 'cdNm': 'Square centimetre', 'userDfnCd1': None}, {'cd': 'm2', 'cdNm': 'Square metre', 'userDfnCd1': None}, {'cd': 'pt', 'cdNm': 'Pints', 'userDfnCd1': None}, {'cd': 'qt', 'cdNm': 'Quarts', 'userDfnCd1': None}, {'cd': 'mm', 'cdNm': 'Millilitre', 'userDfnCd1': None}, {'cd': '2X', 'cdNm': 'Meter/Minute', 'userDfnCd1': None}, {'cd': '4G', 'cdNm': 'Microliter', 'userDfnCd1': None}, {'cd': '4O', 'cdNm': 'Microfarad', 'userDfnCd1': None}, {'cd': '4T', 'cdNm': 'Pikofarad', 'userDfnCd1': None}, {'cd': 'A', 'cdNm': 'Ampere', 'userDfnCd1': None}, {'cd': 'A87', 'cdNm': 'Gigaohm', 'userDfnCd1': None}, {'cd': 'A93', 'cdNm': 'Gram/Cubic meter', 'userDfnCd1': None}, {'cd': 'ACR', 'cdNm': 'Acre', 'userDfnCd1': None}, {'cd': 'B34', 'cdNm': 'Kilogram/cubic decimeter', 'userDfnCd1': None}, {'cd': 'B45', 'cdNm': 'Kilomol', 'userDfnCd1': None}, {'cd': 'B47', 'cdNm': 'Kilonewton', 'userDfnCd1': None}, {'cd': 'B73', 'cdNm': 'Meganewton', 'userDfnCd1': None}, {'cd': 'B75', 'cdNm': 'Megohm', 'userDfnCd1': None}, {'cd': 'B78', 'cdNm': 'Megavolt', 'userDfnCd1': None}, {'cd': 'B84', 'cdNm': 'Microampere', 'userDfnCd1': None}, {'cd': 'BAG', 'cdNm': 'Bag', 'userDfnCd1': None}, {'cd': 'BAR', 'cdNm': 'bar', 'userDfnCd1': None}, {'cd': 'BOT', 'cdNm': 'Bottle', 'userDfnCd1': None}, {'cd': 'BQK', 'cdNm': 'Becquerel/kilogram', 'userDfnCd1': None}, {'cd': 'C10', 'cdNm': 'Millifarad', 'userDfnCd1': None}, {'cd': 'C36', 'cdNm': 'Mol per cubic meter', 'userDfnCd1': None}, {'cd': 'C38', 'cdNm': 'Mol per liter', 'userDfnCd1': None}, {'cd': 'C39', 'cdNm': 'Nanoampere', 'userDfnCd1': None}, {'cd': 'C3S', 'cdNm': 'Cubic centimeter/second', 'userDfnCd1': None}, {'cd': 'C41', 'cdNm': 'Nanofarad', 'userDfnCd1': None}, {'cd': 'C56', 'cdNm': 'Newton/Square millimeter', 'userDfnCd1': None}, {'cd': 'CCM', 'cdNm': 'Cubic centimeter', 'userDfnCd1': None}, {'cd': 'CD', 'cdNm': 'Candela', 'userDfnCd1': None}, {'cd': 'CDM', 'cdNm': 'Cubic decimeter', 'userDfnCd1': None}]}, {'cdCls': '400', 'cdClsNm': 'Taxation Type', 'userDfnNm1': 'Tax Rate', 'dtlList': [{'cd': 'A', 'cdNm': 'Standard Rated(16%)', 'userDfnCd1': '16'}, {'cd': 'B', 'cdNm': 'Minimum Taxable Value (MTV-16%)', 'userDfnCd1': '16'}, {'cd': 'C3', 'cdNm': 'Zero-rated by nature', 'userDfnCd1': '0'}, {'cd': 'D', 'cdNm': 'Exempt', 'userDfnCd1': '0'}, {'cd': 'RVAT', 'cdNm': 'Reverse VAT', 'userDfnCd1': '16'}, {'cd': 'E', 'cdNm': 'Disbursement', 'userDfnCd1': '0'}]}]}}
+"""
 class SyncRequest(Document):
-	# def before_save(self):
-	# 	frappe.throw(self.doc_owner)
-	pass
+	def after_insert(self):
+		# frappe.enqueue(sync_attempt, doc=self, queue='short')
+		sync_attempt(self)
+
+def sync_attempt(doc):	
+	vsdc_response = call_vsdc(doc.end_point, doc.request_data)
+	# print("Sync Request created", r)
+	doc.attempts += 1
+	doc.response_data = vsdc_response
+	doc.status = get_status(vsdc_response)
+	doc.save()
+
 	
+
 	
+
+
+
+
