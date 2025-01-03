896249af37b2351cd4a4fce711fb435ad28479ed
Adam Dawoodjee <adam@Adams-MacBook-Pro.local>
2025-01-03 23:46:31 +0200

    add save_sale offline, test_connection in api.py

--- smart_invoice_api/smart_invoice_api/doctype/sync_request/sync_request.js
+++ smart_invoice_api/smart_invoice_api/doctype/sync_request/sync_request.js
@@ -3,24 +3,33 @@
 
 frappe.ui.form.on("Sync Request", {
 	refresh(frm) {
-        frm.add_custom_button("Sync", () => {
-            frappe.call({
-                method: 'on_update',
-                self: frm.doc
+
+        if (frm.doc.status !== "Success"){
+            let sync = frm.add_custom_button("Sync", () => {
+                frappe.call({
+                    method: 'sync',
+                    doc: frm.doc,
+                    callback: (r=> {
+                        frm.reload_doc()
+                    })
+                });
+
+                frappe.show_alert({
+                    message:__('Synchronizing ...'),
+                    indicator:'green'
+                }, 3);
             });
-        });
+
+            sync.removeClass('btn-default').addClass('btn-success');
+        }
         
         // reset attempts
         frm.add_custom_button("Reset", () => {
-            frm.set_value('attempts', 0); 
+            frm.set_value('attempts', 1); 
+            frm.set_value('status', "New"); 
+            frm.save();
             frm.refresh_field('attempts');
+            frm.refresh_field('status');
         });
-	},
-    attempts(frm) {
-        // Alert Admins after 6 tries
-        if(frm.doc.attempts > 6){
-            //function to alert admins
-            frappe.throw("Contacting Admins")
-        }
-    }
+	}
 });
